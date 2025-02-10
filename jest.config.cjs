d16bd74a60a1f0e62058810a2ac774480af687b6
Ricardo <ricardomorim05@gmail.com>
2025-02-10 18:00:59 +0000

    update Jest configuration and dependencies; add @swc/core and @testing-library/user-event

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,20 +2,18 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest"
   },
-  transformIgnorePatterns: ["/node_modules/"],
   setupFilesAfterEnv: ["<rootDir>/src/test/setupTests.tsx"],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy"
   },
-  // Add these options to suppress the punycode warning
   testEnvironmentOptions: {
-    customExportConditions: ["node", "node-addons"]
+    customExportConditions: [""]
   },
-  // Silence deprecation warnings
   silent: true,
   verbose: false,
   maxWorkers: "50%"
-}
+};
